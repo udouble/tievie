edb0cbed520e0ea46a283609e9b52065e14bbeeb
udouble <Herman.eerdekens@gmail.com>
2025-10-03 20:29:52 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,1 @@
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-self.addEventListener('activate', e => {
-  clients.claim();
-});
-// caching voorlopig uit
+self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>self.clients.claim());self.addEventListener('fetch',e=>{});
