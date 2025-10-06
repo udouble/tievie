2c4f4e4dcbb6a166c9967e070d412012e1f2adb7
udouble <Herman.eerdekens@gmail.com>
2025-10-06 21:28:52 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>self.clients.claim());
+// v3.37-nocache SW
+self.addEventListener('install',(e)=>self.skipWaiting());
+self.addEventListener('activate',(e)=>{
+  e.waitUntil((async()=>{
+    try{const keys=await caches.keys(); await Promise.all(keys.map(k=>caches.delete(k)));}catch(e){}
+    try{await self.registration.unregister();}catch(e){}
+    const cs=await self.clients.matchAll({includeUncontrolled:true});
+    cs.forEach(c=>{try{c.navigate(c.url);}catch(e){}});
+  })());
+});
